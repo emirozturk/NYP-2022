021c321e5fa017b2f573cc10936d661968d908fe
Emir Öztürk <emirozturk3@gmail.com>
2022-02-28 16:13:27 +0300

    İşler biraz düzelmeye başladı

--- Uygulama2/src/com/emirozturk/Main.java
+++ Uygulama2/src/com/emirozturk/Main.java
@@ -2,9 +2,28 @@ package com.emirozturk;
 //Sınıfları asla aynı dosyada yazmayın
 
 class Kisi{
-    String ad;
-    String soyad;
-    byte yas;
+    private String ad;
+    private String soyad;
+    private byte yas;
+
+    public void setAd(String ad){
+        this.ad = ad;
+    }
+    public void setSoyad(String soyad){
+        this.soyad = soyad;
+    }
+    public void setYas(byte yas){
+        this.yas = yas;
+    }
+    public String getAd(){
+        return ad;
+    }
+    public String getSoyad(){
+        return soyad;
+    }
+    public byte getYas(){
+        return yas;
+    }
 }
 
 public class Main {
@@ -12,9 +31,9 @@ public class Main {
     public static void main(String[] args) {
         Kisi yeniKisi;
         yeniKisi = new Kisi();
-        yeniKisi.ad = "Yunus Emre";
-        yeniKisi.soyad = "BATUR";
-        yeniKisi.yas = 20;
-        System.out.println(yeniKisi);
+        yeniKisi.setAd("Yunus Emre");
+        yeniKisi.setSoyad("BATUR");
+        yeniKisi.setYas((byte)20);
+        System.out.println(yeniKisi.getAd() + " "+ yeniKisi.getSoyad() + " "+yeniKisi.getYas());
     }
 }
